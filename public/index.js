1154c60cdfde3bc243ddd7ec9386b233af599ad2
Gem Kosan <gem.kosan77@gmail.com>
2021-07-27 18:19:47 -0500

    adds score, logo & button on a grid

--- public/index.js
+++ public/index.js
@@ -1,20 +1,18 @@
 import lyrics from "./lyrics.js";
 
-const playableWidth = 375;
-const playableHeight = 667;
 const bubblePadding = 15;
 const bubbleDelayMs = 800;
 const floatPxPerSec = 100;
 const bubbleFontFamily = "helvetica";
-const bubbleFontSize = "18px";
+const bubbleFontSize = "16px";
 
 const words = lyrics.split(/\s/);
+
+const playableArea = document.getElementById("playable-area");
+const playableHeight = playableArea.clientHeight;
 const floatTransitionSecs = playableHeight / floatPxPerSec;
 const bubbleStartY = playableHeight;
-const playableAreaStyles = `
-  width: ${playableWidth}px;
-  height: ${playableHeight}px;
-`;
+
 const fontStyles = `
     font-family: ${bubbleFontFamily};
     font-size: ${bubbleFontSize};`;
@@ -23,7 +21,7 @@ const fontStyles = `
 function renderBubble(text, container) {
   let bubble = document.createElement("div");
   let textSpan = document.createElement("span");
-  textSpan.className = "text";
+  textSpan.className = "bubble-text";
   textSpan.innerText = text;
   textSpan.setAttribute("style", fontStyles);
   bubble.className = "bubble";
@@ -52,8 +50,6 @@ function renderBubble(text, container) {
   });
 }
 
-const playableArea = document.getElementById("playable-area");
-playableArea.setAttribute("style", playableAreaStyles);
 const numBubbles = words.length;
 let i = 0;
 const timer = setInterval(function() {
