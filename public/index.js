aeca7ccfa7a5b870a18de5785fea611f7b2d28cb
Gem Kosan <gem.kosan77@gmail.com>
2021-07-28 11:43:59 -0500

    moves styles defs from js to css file

--- public/index.js
+++ public/index.js
@@ -1,29 +1,21 @@
 import lyrics from "./lyrics.js";
 
 const bubblePadding = 15;
-const bubbleDelayMs = 800;
+const bubbleDelayMs = 400;
 const floatPxPerSec = 100;
-const bubbleFontFamily = "helvetica";
-const bubbleFontSize = "16px";
+const maxBubbleScore = 10;
 
-const words = lyrics.split(/\s/);
 
-const playableArea = document.getElementById("playable-area");
-const playableHeight = playableArea.clientHeight;
-const floatTransitionSecs = playableHeight / floatPxPerSec;
-const bubbleStartY = playableHeight;
-
-const fontStyles = `
-    font-family: ${bubbleFontFamily};
-    font-size: ${bubbleFontSize};`;
-
+function stripPunctuation(text) {
+  var punctuation = /[^a-zA-Z'\-\s+]/g;
+  return text.replace(punctuation, "");
+}
 
 function renderBubble(text, container) {
   let bubble = document.createElement("div");
   let textSpan = document.createElement("span");
   textSpan.className = "bubble-text";
   textSpan.innerText = text;
-  textSpan.setAttribute("style", fontStyles);
   bubble.className = "bubble";
   bubble.setAttribute("style", `top: ${bubbleStartY}px;`);
   bubble.append(textSpan);
@@ -44,13 +36,26 @@ function renderBubble(text, container) {
   bubble.addEventListener("transitionend", function({ target }) {
     this.remove();
   });
-  bubble.addEventListener("mousedown", function({ target }) {
-    console.log(`POP! - ${this.innerText}`);
-    this.remove();
-  });
+  bubble.addEventListener("mousedown", e => scoreBubble(e));
 }
 
+function scoreBubble({currentTarget}) {
+  let points = maxBubbleScore - currentTarget.innerText.length + 1;
+  points = points > 0 ? points : 1; 
+  console.log(`POP! - ${currentTarget.innerText} - ${points} points`);
+  currentTarget.remove();
+}
+
+
+/********** Main **********/
+
+const playableArea = document.getElementById("playable-area");
+const playableHeight = playableArea.clientHeight;
+const floatTransitionSecs = playableHeight / floatPxPerSec;
+const bubbleStartY = playableHeight;
+const words = stripPunctuation(lyrics).split(/\s/);
 const numBubbles = words.length;
+
 let i = 0;
 const timer = setInterval(function() {
   while (!words[i] && i < numBubbles) {
