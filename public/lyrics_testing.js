a2d57841742a82ac241f6eac9ede9a2ab2532b61
Gem Kosan <gem.kosan77@gmail.com>
2021-08-02 16:20:28 -0500

    longer test lyrics

--- public/lyrics_testing.js
+++ public/lyrics_testing.js
@@ -1,15 +1,18 @@
 const lyrics = [
-`
-Alright stop, collaborate and listen
-`,
-
-`
-We're no strangers to love
-`,
-
-`
-She was more like a beauty queen from a movie scene
-`
-];
+  `
+  Alright stop, collaborate and listen
+  Ice is back with a brand new invention
+  `,
+  
+  `
+  We're no strangers to love
+  You know the rules and so do I
+  `,
+  
+  `
+  She was more like a beauty queen from a movie scene
+  I said, "Don't mind, but what do you mean, I am the one
+  `
+  ];
 
 export default lyrics;
