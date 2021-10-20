c6c9019c8b7b119dc3518db7975c7ad029056e4b
Gem Kosan <gem.kosan77@gmail.com>
2021-10-19 21:31:35 -0500

    Replaces test lyrics with full lyrics

--- lyrics.js
+++ lyrics.js
@@ -1,9 +1,47 @@
 const lyrics = [
-  `This is the first song`,
-  `This is the second song`,
-  `This is the third song`,
-  `This is the fourth song`,
-  `This is the fifth song`,
-  ];
+`
+Alright stop, collaborate and listen
+Ice is back with a brand new invention
+Something grabs a hold of me tightly
+Flow like a harpoon daily and nightly
+"Will it ever stop?" Yo, I don't know
+Turn off the lights, huh, and I'll glow
+
+Ice, ice, baby
+Vanilla Ice, ice, baby
+`,
+
+`
+We're no strangers to love
+You know the rules and so do I
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy
+
+I just wanna tell you how I'm feeling
+Gotta make you understand
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+`,
+
+`
+She was more like a beauty queen from a movie scene
+I said, "Don't mind, but what do you mean, I am the one
+Who will dance on the floor in the round?"
+She said I am the one
+Who will dance on the floor in the round
+She told me her name was Billie Jean
+As she caused a scene
+Then every head turned with eyes that dreamed of bein' the one
+Who will dance on the floor in the round
+
+Billie Jean is not my lover
+She's just a girl who claims that I am the one
+But the kid is not my son
+`
+];
 
 export default lyrics;
---
+++ lyrics_test.js
@@ -0,0 +1,9 @@
+const lyrics = [
+  `This is the first song`,
+  `This is the second song`,
+  `This is the third song`,
+  `This is the fourth song`,
+  `This is the fifth song`,
+  ];
+
+export default lyrics;
